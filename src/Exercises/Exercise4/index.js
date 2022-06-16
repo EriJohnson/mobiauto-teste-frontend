15f3dcbbf9354dbc4a0fe63bd2c443ef59b6e7cb
Eri JS <erijohnsons.fernandes@gmail.com>
2022-06-16 10:23:03 -0300

    feat: implements exercise 4 solution

--- src/Exercises/Exercise4/index.js
+++ src/Exercises/Exercise4/index.js
@@ -1,7 +1,7 @@
 // Escreva um programa que imprima os números de 1 a 25. Mas para múltiplos de três imprima "Fizz"
 // em vez do número e para os múltiplos de cinco imprima "Buzz". Para números que são múltiplos de três e cinco, imprima "FizzBuzz"
 
-// Ex saida:
+// Ex saída:
 // [
 //     1,          2,      'Fizz',     3,      4,
 //     'Buzz',     5,      'Fizz',     6,      7,
@@ -20,8 +20,29 @@
 //     49,         'Buzz', 50
 //   ]
 
+function isMultiple(multipleOf, value) {
+  return value % multipleOf === 0;
+}
+
+const range = n => [...Array(n + 1).keys()].slice(1);
+
 function fizzBuzz(number) {
   //you code here...
+  let numbers = [];
+
+  for (const iterator of range(number)) {
+    if (isMultiple(3, iterator) && isMultiple(5, iterator)) {
+      numbers.push('FizzBuzz');
+    } else if (isMultiple(3, iterator)) {
+      numbers.push('Fizz');
+    } else if (isMultiple(5, iterator)) {
+      numbers.push('Buzz');
+    }
+
+    numbers.push(iterator);
+  }
+
+  return numbers;
 }
 
 module.exports = fizzBuzz;
