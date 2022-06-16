9c2e8e89e59c6f7a83a97108f2017a4ed3c38936
Eri JS <erijohnsons.fernandes@gmail.com>
2022-06-16 00:10:38 -0300

    feat: implements exercise 3 solution

--- src/Exercises/Exercise3/index.js
+++ src/Exercises/Exercise3/index.js
@@ -1,5 +1,5 @@
 // faça uma chamada rick and morty api e resgate informações do seguintes personagens (Rick Sanchez, Morty Smith, Summer Smith, Beth Smith, Jerry Smith)
-// e ajustar os dados para que fiquem igual a saida de exemplo.
+// e ajustar os dados para que fiquem igual a saída de exemplo.
 // API aberta não precisa de token
 // Documentação
 // https://rickandmortyapi.com/documentation/#rest
@@ -37,8 +37,42 @@
 //   }
 // ]
 
+const axios = require('axios').default;
+
+const BASE_URL = 'https://rickandmortyapi.com/api/character';
+
+const httpClient = axios.create({ baseURL: BASE_URL });
+
+const CHARACTERS = [
+  'Rick Sanchez',
+  'Morty Smith',
+  'Summer Smith',
+  'Beth Smith',
+  'Jerry Smith',
+];
+
+function formatResults(results) {
+  return results.map(character => ({
+    nome: character.name,
+    genero: character.gender === 'Male' ? 'Homem' : 'Mulher',
+    avatar: character.image,
+    especie: 'Humano',
+  }));
+}
+
+function getCharacters(character) {
+  return httpClient.get(`?name=${character}`);
+}
+
 async function getRicAndMortyCharacters() {
-  //you code here...
+  return Promise.all(
+    CHARACTERS.map(character => getCharacters(character))
+  ).then(results => {
+    const characters = results.map(result => result.data.results[0]);
+    const formattedResults = formatResults(characters);
+
+    return formattedResults;
+  });
 }
 
 module.exports = getRicAndMortyCharacters;
