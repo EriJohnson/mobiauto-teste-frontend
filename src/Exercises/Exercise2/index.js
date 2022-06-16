237bcb2587c40c77689b33305dcf4df1cc4c9db2
Eri JS <erijohnsons.fernandes@gmail.com>
2022-06-15 21:52:16 -0300

    feat: implements exercise 2 solution

--- src/Exercises/Exercise2/index.js
+++ src/Exercises/Exercise2/index.js
@@ -1,18 +1,27 @@
-// faça uma função que recebe um objeto como primeiro parametro e como segundo parametro um objeto com dados que vão atualizar o objeto do primeiro parametro
-// Obs: se no objeto do segundo parametro tiver dados que o objeto do primeiro não tem, o valor não deve persistir no objeto de retorno da função, somente
-// é atualizado os dados que o objeto do primeiro parametro possuir.
+// faça uma função que recebe um objeto como primeiro parâmetro e como segundo parâmetro um objeto com dados que vão atualizar o objeto do primeiro parâmetro
+// Obs: se no objeto do segundo parâmetro tiver dados que o objeto do primeiro não tem, o valor não deve persistir no objeto de retorno da função, somente
+// é atualizado os dados que o objeto do primeiro parâmetro possuir.
 
 // Ex: updateData({ name: "Marcos", country: "Brasil", age: 22 }, { country: "Japão", age: 33 })
-// --> saida: { name: 'Marcos', country: 'Japão', age: 33 }
+// --> saída: { name: 'Marcos', country: 'Japão', age: 33 }
 
 // Ex: updateData({ name: "Marcos", country: "Brasil", age: 22 }, { price: 89.9, amount: 15, description: "camiseta 100% algodão" })
-// --> saida: { name: "Marcos", country: "Brasil", age: 22 }
+// --> saída: { name: "Marcos", country: "Brasil", age: 22 }
 
 // Ex: updateData({ name: "Rafael", country: "Chile", age: 42 }, { name: "Camiseta Polo", price: 59.9, amount: 30 })
-// --> saida: { name: "Camiseta Polo", price: 59.9, amount: 30 }
+// --> saída: { name: "Camiseta Polo", price: 59.9, amount: 30 }
 
 function updateData(currentObject, newDataObject) {
   //you code here...
+  const updatedObject = currentObject;
+
+  for (const key in newDataObject) {
+    if (key in currentObject) {
+      updatedObject[key] = newDataObject[key];
+    }
+  }
+
+  return updatedObject;
 }
 
 module.exports = updateData;
